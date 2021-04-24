9b34485912cffee29c9586e9851d25c1fa7031cc
Josh Mak <jmak2015@gmail.com>
2021-04-23 21:39:53 -0700

    Separate logic out into functions

--- src/mya.c
+++ src/mya.c
@@ -20,14 +20,42 @@ static struct argp_option options[] = {
 	{ 0 }
 };
 
-size_t MYA_MIN_USERNAME_LENGTH = 2;
-size_t MYA_MAX_USERNAME_LENGTH = 16;
-size_t JIKAN_PAGE_SIZE = 300;
+const size_t MYA_MIN_USERNAME_LENGTH = 2;
+const size_t MYA_MAX_USERNAME_LENGTH = 16;
+const size_t JIKAN_PAGE_SIZE = 300;
 
 struct arguments {
 	enum { WATCHING_MODE, COMPLETED_MODE, HOLD_MODE, DROPPED_MODE, PLAN_MODE, ALL_MODE } mode;
 	char *args[1];
 };
+
+void validate_username (char *username) {
+	char username_buf[MYA_MAX_USERNAME_LENGTH+2];
+	strncpy(username_buf, username, sizeof(username_buf)-1);
+	username_buf[sizeof(username_buf)-1] = '\0';
+	size_t username_len = strlen(username_buf);
+	regex_t regex;
+	char *pattern = "^[a-zA-Z0-9_-]+$";
+	size_t nmatch = 1;
+	regmatch_t pmatch[1];
+
+	if (username_len < MYA_MIN_USERNAME_LENGTH || username_len > MYA_MAX_USERNAME_LENGTH) {
+		fprintf(stderr, "Username must be between %zu and %zu characters in length\n", MYA_MIN_USERNAME_LENGTH, MYA_MAX_USERNAME_LENGTH);
+		exit(argp_err_exit_status);
+	}
+
+	if (regcomp(&regex, pattern, REG_EXTENDED)) {
+		fprintf(stderr, "Failed to compile username validation regex\n");
+		exit(argp_err_exit_status);
+	}
+
+	if (regexec(&regex, username, nmatch, pmatch, 0)) {
+		fprintf(stderr, "Please enter a valid username (letters, numbers, underscores and dashes only)\n");
+		exit(argp_err_exit_status);
+	}
+
+	regfree(&regex);
+}
 
 static error_t parse_opt (int key, char *arg, struct argp_state *state) {
 	struct arguments *arguments = state->input;
@@ -40,33 +68,7 @@ static error_t parse_opt (int key, char *arg, struct argp_state *state) {
 	case 'a': arguments->mode = ALL_MODE; break;
 	case ARGP_KEY_ARG:
 		if (state->arg_num >= 1) argp_usage(state);
-		{
-			char username[MYA_MAX_USERNAME_LENGTH+2];
-			strncpy(username, arg, sizeof(username)-1);
-			username[sizeof(username)-1] = '\0';
-			size_t arg_len = strlen(username);
-			regex_t regex;
-			char *pattern = "^[a-zA-Z0-9_-]+$";
-			size_t nmatch = 1;
-			regmatch_t pmatch[1];
-
-			if (arg_len < MYA_MIN_USERNAME_LENGTH || arg_len > MYA_MAX_USERNAME_LENGTH) {
-				printf("Username must be between %zu and %zu characters in length\n", MYA_MIN_USERNAME_LENGTH, MYA_MAX_USERNAME_LENGTH);
-				exit(argp_err_exit_status);
-			}
-
-			if (regcomp(&regex, pattern, REG_EXTENDED)) {
-				fprintf(stderr, "Failed to compile username validation regex\n");
-				exit(argp_err_exit_status);
-			}
-
-			if (regexec(&regex, arg, nmatch, pmatch, 0)) {
-				printf("Please enter a valid username (letters, numbers, underscores and dashes only)\n");
-				exit(argp_err_exit_status);
-			}
-
-			regfree(&regex);
-		}
+		validate_username(arg);
 		arguments->args[state->arg_num] = arg;
 		break;
 	case ARGP_KEY_END:
@@ -123,97 +125,108 @@ CURLcode curl_fetch_url (CURL *curl, const char *url, struct curl_fetch_st *fetc
 	return curl_easy_perform(curl);
 }
 
+void generate_endpoint (char *endpoint, size_t mode) {
+	switch (mode) {
+	case ALL_MODE: strcpy(endpoint, "all"); break;
+	case COMPLETED_MODE: strcpy(endpoint, "completed"); break;
+	case HOLD_MODE: strcpy(endpoint, "onhold"); break;
+	case DROPPED_MODE: strcpy(endpoint, "dropped"); break;
+	case PLAN_MODE: strcpy(endpoint, "plantowatch"); break;
+	default: strcpy(endpoint, "watching"); break;
+	}
+}
+
+void generate_anime_api_uri (char *uri, char *username, char *endpoint) {
+	strcpy(uri, "https://api.jikan.moe/v3/user/");
+	strcat(uri, username);
+	strcat(uri, "/animelist/");
+	strcat(uri, endpoint);
+	strcat(uri, "?order_by=title&sort=desc");
+}
+
+void generate_paginated_uri (char *paginated_uri, char *base_uri, short page) {
+	char page_buf[2];
+	sprintf(page_buf, "%hu", page);
+	strcpy(paginated_uri, base_uri);
+	strcat(paginated_uri, "&page=");
+	strcat(paginated_uri, page_buf);
+}
+
+struct curl_fetch_st get_anime_list (char *paginated_uri) {
+	CURL *curl = curl_easy_init();
+
+	if (!curl) {
+		fprintf(stderr, "Curl init failed\n");
+		exit(EXIT_FAILURE);
+	}
+
+	struct curl_fetch_st curl_fetch;
+	struct curl_fetch_st *cf = &curl_fetch;
+	CURLcode res = curl_fetch_url(curl, paginated_uri, cf);
+	curl_easy_cleanup(curl);
+
+	if (res != CURLE_OK) {
+		fprintf(stderr, "API fetch error: %s\n", curl_easy_strerror(res));
+		free(cf->payload);
+		exit(EXIT_FAILURE);
+	}
+
+	return curl_fetch;
+}
+
+size_t print_anime_list (struct json_object *anime_list, short page, char *list_name) {
+	size_t n_anime = json_object_array_length(anime_list);
+
+	if (page == 1) {
+		if (n_anime == JIKAN_PAGE_SIZE) {
+			printf("%s %lu+ anime\n", list_name, JIKAN_PAGE_SIZE);
+		} else {
+			printf("%s %lu anime\n", list_name, n_anime);
+		}
+	}
+
+	for (size_t i = 0; i < n_anime; i++) {
+		struct json_object *anime = json_object_array_get_idx(anime_list, i);
+		struct json_object *anime_json = json_tokener_parse(json_object_get_string(anime));
+		struct json_object *anime_title;
+		json_object_object_get_ex(anime_json, "title", &anime_title);
+		printf("%lu. %s\n", (i+1)+(JIKAN_PAGE_SIZE*(page-1)), json_object_get_string(anime_title));
+	}
+
+	return n_anime;
+}
+
 int main (int argc, char *argv[]) {
 	struct arguments arguments;
 	arguments.mode = WATCHING_MODE;
 	argp_parse(&argp, argc, argv, 0, 0, &arguments);
 
-	char *endpoint;
-	switch (arguments.mode) {
-	case ALL_MODE: endpoint = "all"; break;
-	case COMPLETED_MODE: endpoint = "completed"; break;
-	case HOLD_MODE: endpoint = "onhold"; break;
-	case DROPPED_MODE: endpoint = "dropped"; break;
-	case PLAN_MODE: endpoint = "plantowatch"; break;
-	default: endpoint = "watching"; break;
-	}
+	char endpoint[12], base_uri[94];
+	generate_endpoint(endpoint, arguments.mode);
+	generate_anime_api_uri(base_uri, arguments.args[0], endpoint);
 
-	char base_url[249];
-	strcpy(base_url, "https://api.jikan.moe/v3/user/");
-	strcat(base_url, arguments.args[0]);
-	strcat(base_url, "/animelist/");
-	strcat(base_url, endpoint);
-	strcat(base_url, "?order_by=title&sort=desc&");
+	short page_num = 0;
 
-	short page_num = 1;
-	char page[2], paginated_url[256];
+	while (++page_num) {
+		char paginated_uri[102];
+		generate_paginated_uri(paginated_uri, base_uri, page_num);
 
-	CURL *curl;
-	CURLcode res;
-	struct curl_fetch_st curl_fetch;
-	struct curl_fetch_st *cf = &curl_fetch;
+		struct curl_fetch_st curl_fetch = get_anime_list(paginated_uri);
+		struct json_object *json = json_tokener_parse(curl_fetch.payload);
+		free(curl_fetch.payload);
 
-	struct json_object *anime_list, *anime_json, *anime, *anime_title, *json;
-	size_t n_anime;
-	json_bool is_user_exist;
+		struct json_object *anime_list;
 
-	while (1) {
-		sprintf(page, "%d", page_num);
-
-	       	strcpy(paginated_url, base_url);
-		strcat(paginated_url, "page=");
-		strcat(paginated_url, page);
-
-		curl = curl_easy_init();
-
-		if (!curl) {
-			fprintf(stderr, "Curl init failed\n");
-			return EXIT_FAILURE;
-		}
-
-		res = curl_fetch_url(curl, paginated_url, cf);
-		curl_easy_cleanup(curl);
-
-		if (res != CURLE_OK) {
-			fprintf(stderr, "API fetch error: %s\n", curl_easy_strerror(res));
-			free(cf->payload);
-			return EXIT_FAILURE;
-		}
-
-		json = json_tokener_parse(cf->payload);
-		free(cf->payload);
-
-		is_user_exist = json_object_object_get_ex(json, "anime", &anime_list);
-
-		if (!is_user_exist) {
+		if (!json_object_object_get_ex(json, "anime", &anime_list)) {
 			fprintf(stderr, "User not found\n");
-			return EXIT_FAILURE;
+			exit(EXIT_FAILURE);
 		}
-
-		n_anime = json_object_array_length(anime_list);
-
-		if (page_num == 1) {
-			if (n_anime == JIKAN_PAGE_SIZE) {
-				printf("%s %lu+ anime\n", endpoint, JIKAN_PAGE_SIZE);
-			} else {
-				printf("%s %lu anime\n", endpoint, n_anime);
-			}
-		}
-
-		for (size_t i = 0; i < n_anime; i++) {
-			anime = json_object_array_get_idx(anime_list, i);
-			anime_json = json_tokener_parse(json_object_get_string(anime));
-
-			json_object_object_get_ex(anime_json, "title", &anime_title);
-			printf("%lu. %s\n", (i+1)+(JIKAN_PAGE_SIZE*(page_num-1)), json_object_get_string(anime_title));
-		}
+		
+		size_t num_anime = print_anime_list(anime_list, page_num, endpoint);
 
 		json_object_put(json);
-
-		if (n_anime < JIKAN_PAGE_SIZE) break;
-
-		page_num += 1;
+		if (num_anime < JIKAN_PAGE_SIZE) break;
 	}
-
 	return EXIT_SUCCESS;
 }
+
